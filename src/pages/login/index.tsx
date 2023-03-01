a29e2b8e33b43da818020e885818ef7debcd05cf
Pedro256 <pedroh93601@gmail.com>
2023-03-01 16:46:14 -0400

    fix: login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -5,18 +5,18 @@ import { Button } from '@/components/button';
 
 export default function Login() {
 
-  const [email,setEmail]= useState("");
-  const [password,setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className={styles.bglogin}>
       <div className='block md:flex md:flex-row-reverse'>
-      <div className='w-full md:w-1/2 flex justify-center'>
-          <img src="favico.svg" alt="logo" className='w-2/5'/>
+        <div className='w-full md:w-1/2 flex justify-center'>
+          <img src="favico.svg" alt="logo" className='w-2/5' />
         </div>
         <div className={styles.card}>
-          
-          <h1 className="text-base  font-bold sm:text-2xl">
+
+          <h1 className="text-xl font-bold sm:text-4xl">
             Entrar
           </h1>
           <p>Acesse as rifas usando email e senha cadastrado .</p>
@@ -32,28 +32,28 @@ export default function Login() {
               <a className="text-link-esp" href="#">Esqueci a senha !</a>
             </div>
           </div>
-          
+
           <div>
             <label htmlFor="pass">Password: </label>
             <div className="w-3/4">
               <Input name='pass' type="password" value={password} setValue={setPassword}></Input>
             </div>
           </div>
-          
-          <Button value="Prosseguir" type="primary"/>
+
+          <Button value="Prosseguir" type="primary" />
 
           <div className='grid justify-items-end w-3/4'>
             <div>
-              <p> Caso não tenha 
-              <a className="text-link-esp" href="#"> Crie uma Conta !</a>
+              <p> Caso não tenha
+                <a className="text-link-esp" href="register"> Crie uma Conta !</a>
               </p>
-              
+
             </div>
           </div>
-          
+
 
         </div>
-        
+
       </div>
     </div>
   )

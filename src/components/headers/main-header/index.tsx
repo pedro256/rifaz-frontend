7e19e3675d1bb79a1485f36f04e1134d55d2184f
Pedro256 <pedroh93601@gmail.com>
2023-03-01 23:54:58 -0400

    fix: header responsive

--- src/components/headers/main-header/index.tsx
+++ src/components/headers/main-header/index.tsx
@@ -1,39 +1,142 @@
 import style from './index.module.css'
-import {AiOutlineMenu,AiOutlineClose } from 'react-icons/ai'
-import {useState} from 'react'
+import { AiOutlineMenu, AiOutlineClose,AiOutlineSearch } from 'react-icons/ai'
+import { useState } from 'react'
+import { Input } from '@/components/input'
+
+interface IItemNav {
+    title: string,
+    url: string,
+    subNav?: boolean,
+    subNavItem?: Array<IItemNav>
+}
 
 export function MainHeader() {
 
-    const [isOpenMenu,setIsOpenMenu] = useState(false)
+    const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false)
+    const [selectHeader,setSelectedHeader] = useState<string>("");
+    const navItem: Array<IItemNav> = [
+        {
+            title: "Nova Rifa",
+            url: "rifa/novo",
+        },
+        {
+            title: "Avisos",
+            url: "#"
+        },
+        {
+            title: "Opções",
+            url: "#",
+            subNav: true,
+            subNavItem: [
+                {
+                    title: "Meu Perfil",
+                    url: "profile"
+                },
+                {
+                    title: "Nova Rifa",
+                    url: "rifa/novo"
+                },
+                {
+                    title: "Minhas Rifas",
+                    url: "rifa/my"
+                },
+                {
+                    title: "Rifas Compradas",
+                    url: "rifa/bought"
+                }
+            ]
+        }
+    ]
 
     return (
         <nav className={style.navbarBg}>
             <div className="flex justify-between px-6 pt-4">
                 <div className='w-full md:w-auto flex justify-between'>
                     <img src="favico.svg" alt="logo" className="w-12" />
-                    <div className='md:hidden'  onClick={()=>setIsOpenMenu(!isOpenMenu)}>
+                    <div className='md:hidden' onClick={() => setIsOpenMenu(!isOpenMenu)}>
                         {
-                            isOpenMenu?
-                            (<AiOutlineClose size={30}></AiOutlineClose>):
-                             (<AiOutlineMenu size={30} ></AiOutlineMenu>)
-                             
+                            isOpenMenu ?
+                                (<AiOutlineClose size={30}></AiOutlineClose>) :
+                                (<AiOutlineMenu size={30} ></AiOutlineMenu>)
+
                         }
-                        
+
                     </div>
                 </div>
-                <div>
+                <div className='pr-10'>
                     <ul className="md:flex hidden gap-8">
-                        <li className='font-semibold px-5'>Nova Rifas</li>
-                        <li className='font-semibold px-5'>Avisos</li>
-                        <li className='font-semibold px-5'>Perfil</li>
+                        {
+                            navItem.map(x => (
+                                <li key={x.title} className='font-semibold px-5 group'>
+                                    <div>
+                                        <a href={x.url}>{x.title}</a>
+                                        {
+                                            x.subNav ? (
+                                                <div className='absolute top-15 hidden group-hover:block hover:block'>
+                                                    <div className='bg-white px-3 py-4 drop-shadow-md'>
+                                                        <div className='divide-solid divide-y'>
+                                                            {
+                                                                x.subNavItem?.map(sub => (
+                                                                    <div key={sub.title} className='text-sm text-gray-600 my-2'>
+                                                                        <a className='hover:text-secundary' href={sub.url}>{sub.title}</a>
+                                                                    </div>
+                                                                ))
+                                                            }
+                                                        </div>
+
+                                                    </div>
+
+                                                </div>
+
+                                            ) : null
+                                        }
+                                    </div>
+                                </li>
+                            ))
+                        }
                     </ul>
                 </div>
+
             </div>
-            <div className={`${style.ulOptions} duration-500 ${isOpenMenu? "left-0":"left-[-100%]"}`}>
+            <div className='flex justify-center'>
+                <div className='py-4 flex items-center'>
+                    <input type="text" />
+                    <AiOutlineSearch></AiOutlineSearch>
+                    <button>Filtro</button>
+                </div>
+            </div>
+            <div className={`${style.sideNavBar} duration-500 ${isOpenMenu ? "left-0" : "left-[-100%]"}`}>
                 <ul>
-                    <li className='font-semibold px-5 py-6'>Nova Rifas</li>
-                    <li className='font-semibold px-5 py-6'>Avisos</li>
-                    <li className='font-semibold px-5 py-6'>Perfil</li>
+                    {
+                        navItem.map(x => (
+                            <li key={x.title} className='font-semibold px-5 py-6'>
+                                <div>
+                                    {
+                                        (!x.subNav)?
+                                        (<a href={x.url}> {x.title}</a>):
+                                        (<h3 onClick={e => selectHeader==x.title?setSelectedHeader("") :setSelectedHeader(x.title)}>{x.title}</h3>)
+                                    }
+                                    {
+                                        <div className={
+                                            `${selectHeader === x.title? 'block':'hidden'} bg-white px-2 py-4 rounded-sm`
+                                        }>
+                                            {
+                                                x.subNavItem?.map(
+                                                    sub =>(
+                                                        <div key={sub.title}>
+                                                            <a href={sub.url}>{sub.title}</a>
+                                                        </div>
+                                                    )
+                                                )
+                                            }
+
+                                        </div>
+                                        
+                                    }
+                                </div>
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
 

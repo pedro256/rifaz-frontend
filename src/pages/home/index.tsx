4fe159f39bd4214ce7f6834208c08a412a21c278
Pedro256 <pedroh93601@gmail.com>
2023-03-06 12:52:52 -0400

    fix: pre imp home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,34 @@
 import { MainHeader } from "@/components/headers/main-header"
+import { useState } from 'react'
+import { ShowCaseRaffleCard } from "@/components/card/raffle/showcase"
 
-export default function Home(){
-    return(
+export default function Home() {
+    const [search, setSearch] = useState<string>("")
+    return (
         <>
-            <MainHeader></MainHeader>
-            <h1>teste</h1>
+            <MainHeader search={search} setSearch={setSearch}></MainHeader>
+            <section>
+                <div className="flex justify-center">
+                    <div className="w-full mt-5 m-3 md:m-1 md:mt-6 md:max-w-screen-lg">
+
+                        <div className="pl-2 pb-6 md:pl-6">
+                            <span className="font-semibold italic">mais de 100 rifas encontradas</span>
+                        </div>
+
+
+                        <ShowCaseRaffleCard
+                        title="Primeira Rifa Do Amigos"
+                        description="vamos de descriçãozinha"
+                        prizesDescription={[
+                            "Teste",
+                            "Outro"
+                        ]}
+                        value={23.3442}
+                        />
+                    </div>
+                </div>
+
+            </section>
         </>
     )
 }

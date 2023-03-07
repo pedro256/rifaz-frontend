cdd05f39d6ce9341b1c72e52656d9d863edb9886
Pedro256 <pedroh93601@gmail.com>
2023-03-07 11:57:59 -0400

    fix: header configuration

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,51 @@
-import { MainHeader } from "@/components/headers/main-header"
+import { IItemNav, MainHeader } from "@/components/headers/main-header"
 import { useState } from 'react'
 import { ShowCaseRaffleCard } from "@/components/card/raffle/showcase"
 
 export default function Home() {
-    const [search, setSearch] = useState<string>("")
+    const [search, setSearch] = useState<string>("");
+    const navItem: Array<IItemNav> = [
+        {
+            title: "Nova Rifa",
+            url: "rifa/novo",
+        },
+        {
+            title: "Avisos",
+            url: "#"
+        },
+        {
+            title: "Opções",
+            url: "#",
+            subNav: true,
+            subNavItem: [
+                {
+                    title: "Meu Perfil",
+                    url: "profile"
+                },
+                {
+                    title: "Nova Rifa",
+                    url: "rifa/novo"
+                },
+                {
+                    title: "Minhas Rifas",
+                    url: "rifa/my"
+                },
+                {
+                    title: "Rifas Compradas",
+                    url: "rifa/bought"
+                }
+            ]
+        }
+    ];
+
     return (
         <>
-            <MainHeader search={search} setSearch={setSearch}></MainHeader>
+            <MainHeader
+            habSearcher={true}
+            search={search} 
+            setSearch={setSearch}
+            navItem={navItem}
+            ></MainHeader>
             <section>
                 <div className="flex justify-center">
                     <div className="w-full mt-5 m-3 md:m-1 md:mt-6 md:max-w-screen-lg">
@@ -17,13 +56,23 @@ export default function Home() {
 
 
                         <ShowCaseRaffleCard
-                        title="Primeira Rifa Do Amigos"
-                        description="vamos de descriçãozinha"
-                        prizesDescription={[
-                            "Teste",
-                            "Outro"
-                        ]}
-                        value={23.3442}
+                            title="Primeira Rifa Do Amigos"
+                            description="vamos de descriçãozinha"
+                            prizesDescription={[
+                                "Teste",
+                                "Outro"
+                            ]}
+                            value={23.3442}
+                        />
+
+                        <ShowCaseRaffleCard
+                            title="Primeira Rifa Do Amigos"
+                            description="vamos de descriçãozinha 3h3y3yy ieie infime eieimrr  ejeiieb"
+                            prizesDescription={[
+                                "Teste",
+                                "Outro"
+                            ]}
+                            value={23.3442}
                         />
                     </div>
                 </div>

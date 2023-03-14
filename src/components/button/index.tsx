cd1b8edfccab7524f3215edc8b6639299d2010b8
Pedro256 <pedroh93601@gmail.com>
2023-03-14 16:26:58 -0400

    fix: buttom style

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -4,9 +4,13 @@ interface Props{
     name?:string,
     id?:string,
     value:any,
-    type?:string
+    type?:'primary' | 'primaryBlack' | 'secondary'|'black'|'blank' | undefined,
+    typeButtom?: 'submit' | 'reset' | 'button' | undefined;
+    className?:string
+    onClick?:Function
 
 }
+
 export function Button(props:Props){
 
     const getStyle = ():string=>{
@@ -21,7 +25,12 @@ export function Button(props:Props){
             case "secondary":
                 stl += style.btnStyleSecondary;
                 break;
-            
+            case "black":
+                stl += style.btnStyleBlack;
+                break;
+            case "blank":
+                stl += style.btnStyleBlank;
+                break;
         }
 
         return  stl;
@@ -32,6 +41,8 @@ export function Button(props:Props){
         className={getStyle()}
         name={props.name}
         id={props.id}
+        onClick={()=> props.onClick? props.onClick() : null}
+        type={props.typeButtom || "button"}
         >
             {props.value}
         </button>
